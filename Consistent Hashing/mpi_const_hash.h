6ea4eb41f54873155c6597f07b2caf44b83eac1f
Ykb-bh <56471730+Ykb-bh@users.noreply.github.com>
2020-05-12 11:34:24 +0200

    Update mpi_const_hash.h

--- Consistent Hashing/mpi_const_hash.h
+++ Consistent Hashing/mpi_const_hash.h
@@ -15,22 +15,29 @@ typedef struct key_Vsize_s{
 
 typedef char* Value_Buff_t  ;
 
+void build_data_type(MPI_Datatype *MPI_Key_vsize);
 
 /*--------------------------------------- Clients side ----------------------------------------*/
 
-int MPI_Client_set_data(char* key, char* value, size_t in_data, node_t* Server_ring_g) ;
+int MPI_Client_set_data(char* key, char* value, size_t in_data, node_t* Server_ring_g, int limit_hash_space) ;
 
-int MPI_Client_get_data(char* key, char** value, size_t* out_data, node_t* Server_ring_g);
+int MPI_Client_get_data(char* key, char** value, size_t* out_data, node_t* Server_ring_g, int limit_hash_space);
 
-int MPI_Finish_task();
+int MPI_Finish_task(int nbr_servers, int nproc, int target_client, int part);
 
 
 /*-------------------------------------- Servers Side -------------------------------------------*/
 
 /* initilize the ring of servers : processes with even rank (rank %2 = 0) */
-int MPI_Initilize_server_ring(local_kvs_t* local_kvs_root/*, int rank, node_t** Server_ring_g*/);
-void Initialize_ring(node_t** Server_ring_g   /*, int *glob */);
 
-int MPI_Server_process_requests(local_kvs_t *local_kvs_root);
+void Initialize_ring(node_t** Server_ring_g   , int nbr_server, int nproc );
 
+int MPI_Server_process_requests(local_kvs_t **local_kvs_root, double *elapsed_time, long int *nbr_opr);
+
+
+void inorder_send(local_kvs_t** root, uint32_t, int id_piv, MPI_Datatype MPI_Key_vsize);
+
+int mode_data(uint32_t s_src, uint32_t s_dest, local_kvs_t **local_kvs_root, int id_piv);
+
+int MPI_Refresh_ring(node_t **Server_ring_g, int *msg_bcast, int chunk, int *nbr_servers, int part, local_kvs_t** local_kvs_root, int limit_hash_space);
 #endif
