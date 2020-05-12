337537a1d160e6d31e010fba8a346d3a01a49082
TRABZI <57754442+TRABZI@users.noreply.github.com>
2020-05-12 10:50:46 +0200

    Update Consistent_Hashing.c

--- Consistent Hashing/Consistent_Hashing.c
+++ Consistent Hashing/Consistent_Hashing.c
@@ -3,13 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+//#include "murmur3.h"
+#include <string.h>
+//#include <mpi_const_hash.h>
 
 /* GLobal BST structure that represents the ring of servers . 
 * Initialized in mpi_const_hash.c  : 
 * Voir MPI_Initilize_server_ring(local_kvs_t** local_kvs_root, int rank)
 */
 
- 
+#define SEED 42 
 
 
 /* node_ID représente la valeur hash du serveur obtenu par la fonction de hashage murmurHash*/
@@ -97,17 +100,17 @@ node_t* remove_server(node_t* root, uint32_t to_remove){
 
 
 
-int search_server(node_t* root, uint32_t server_id, node_t ** target_node){
+int search_server(node_t* root, int server_rank, node_t ** target_node){
 	if (root == NULL )
 		return 0;
-	else if(root->node_ID == server_id){
+	else if(root->true_rank == server_rank){
 		*target_node = root;
 		return 1 ;
 	}
-	else if (server_id > root->node_ID)
-		search_server(root->right , server_id, target_node);
-	else if (server_id < root->node_ID)
-		search_server(root->left  , server_id, target_node);
+	else if (server_rank > root->true_rank)
+		search_server(root->right , server_rank, target_node);
+	else if (server_rank < root->true_rank)
+		search_server(root->left  , server_rank, target_node);
 	else
 		return 0 ;
 }
@@ -119,40 +122,113 @@ trouver le successeur d'un serveur :
  *  pour pouvoir copier les éléments du successeur correspondants au nouveau serveur
  *  ou de copier les élément de serveur supprimé vers le successeur
 */
-int find_successor(node_t *root, uint32_t id, int *rank_successor){
-	if (root == NULL || (root->left == NULL && root->right == NULL) ){
-		*rank_successor =  -1 ;
+int find_successor(node_t *root, node_t* root_, uint32_t id, int *rank_successor, int *id_successor){
+	if (root == NULL) {
+		*rank_successor = -1 ;
+		*id_successor	= -1 ;
 		return 1 ;
+	}
+	else if (id == root->node_ID){
+		*rank_successor = root->true_rank ;
+		*id_successor	= root->node_ID ;
+		return 0;
+	}
+	else if (root->left == NULL || root->right == NULL){
+		if ( root->right != NULL && id > root->node_ID ){			
+			node_t* tmp = root->right ;
+			if( id <= tmp->node_ID){
+				*rank_successor = tmp->true_rank ;
+				*id_successor	= tmp->node_ID ;
+				return 0 ;
+			}
+			else 
+				find_successor(tmp, root_, id, rank_successor, id_successor);	
+		}
+		else if ( root->left != NULL && id < root->node_ID ){
+			node_t* tmp = root->left ;
+			if ( id <= tmp->node_ID){
+				*rank_successor = tmp->true_rank ;
+				*id_successor	= tmp->node_ID ;
+				return 0;
+			}
+			else
+				find_successor(tmp, root_, id, rank_successor, id_successor);
+			}
+		else{	
+			node_t *target_node = NULL;
+	 		find_low_id( root_ , &target_node);
+			*rank_successor	= root_ -> true_rank ;
+			*id_successor	= root_ -> node_ID ;
+			return 0;
+		}	
 	}
 	else if (id > root->node_ID) {
 		node_t* tmp = root->right ;
 		if( id <= tmp->node_ID){
 			*rank_successor = tmp->true_rank ;
+			*id_successor	= tmp->node_ID ;
 			return 0 ;
 		}
 		else 
-			find_successor(tmp, id, rank_successor);	
+			find_successor(tmp, root_, id, rank_successor, id_successor);	
 	}
 	else if(id < root->node_ID){
 		node_t* tmp = root->left ;
 		if ( id <= tmp->node_ID){
 			*rank_successor = tmp->true_rank ;
+			*id_successor	= tmp->node_ID ;
 			return 0;
 		}
 		else
-			find_successor(tmp, id, rank_successor);
+			find_successor(tmp, root_, id, rank_successor, id_successor);
+	}
+	else{
+		//node_t *target_node = NULL;
+	 	//find_low_id(root, &target_node);
+		//*rank_successor = root -> true_rank;
 	}
 	return 1;		
 }
 
 
-/*
+
 void Server_BST_display(node_t *root){
 	if (root != NULL){
-		BST_display(root->left);
-		printf(" %d ", root->node_ID);
-		BST_display(root->right);
+		Server_BST_display(root->left);
+		printf("true_rank = %d\t", root->true_rank);fflush(stdout);
+		printf("hash = %ud ", root->node_ID);
+		Server_BST_display(root->right);
 	}
-	//printf("\n");
+	printf("\n");//fflush(stdout);
+}
+
+/*
+node_t* root = NULL;
+
+int main(){
+*/
+	
+/*	for(int rank = 0; rank < 4; rank++){
+		//int rank = 3 ;
+		uint32_t server_hash_id = 0;
+        	char server [100];
+        	//printf("server = %s\n", server);
+        	sprintf(server, "server with rank %d", rank);
+
+        	//printf("server = %s length = %ld\n", server, strlen(server));
+        	int len = strlen(server);
+        	uint32_t seed = SEED;
+        	MurmurHash3_x86_32(server, len, seed, &server_hash_id);
+		//printf();
+		root = add_server(root, rank, server_hash_id);
+
+	}
+*/
+	//root = add_server(root, 0, 0);
+//	Server_BST_display(root);
+
+/*	Initialize_ring(&root);
+
+	return 0 ;
 }*/
 
